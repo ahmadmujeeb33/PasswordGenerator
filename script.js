b5f90a950fdc97990e596e4a1bbe9b657ed24991
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-05-20 20:28:20 -0400

    function has mostly been built

--- script.js
+++ script.js
@@ -15,43 +15,32 @@ function writePassword() {
 //   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  
-
   let allItems = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','1','2','3','4','5','6','7','8','9','!','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','',']','^','_','`','{','|','}','~']
   let allItemsDuplicate = allItems;
+  let store = [];
 
-
-  console.log(wantsSpecilCharacters)
-
-  if(!wantsSpecilCharacters){
-      console.log("11111111111111");
-      allItemsDuplicate.splice(60)
-      console.log(allItemsDuplicate);
+  if(wantsSpecilCharacters){
+      store = store.concat(allItemsDuplicate.slice(61))
   }
 
-  if(!wantsLowercase){
-      console.log("22222222222");
-      allItemsDuplicate.splice(0,26)
-      console.log(allItemsDuplicate);
+  if(wantsLowercase){
+      store = store.concat(allItemsDuplicate.slice(0,26));
+      
   }
 
-  if(!wantsUppercase){
-      console.log("33333333333333");
-      allItemsDuplicate.splice(26,26)
-      console.log(allItemsDuplicate);
+  if(wantsUppercase){
+      store = store.concat(allItemsDuplicate.slice(26,52))
+      
   }
 
-  if(!wantNumbers){
-      console.log("444444444444444");
-      allItemsDuplicate.splice(51,9);
-      console.log(allItemsDuplicate);
+  if(wantNumbers){
+      store = store.concat(allItemsDuplicate.slice(52,61))
+      
   }
 
-  console.log("length " + allItemsDuplicate.length)
   for(let i=0; i<parseInt(wordCount);i++){
-    let index = Math.floor((Math.random() * allItemsDuplicate.length-1) + 1);
-    console.log(index);
-    password = password + allItemsDuplicate[index];
+    let index = Math.floor((Math.random() * store.length -1) + 1);
+    password = password + store[index];
 
   }
 
