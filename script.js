82f4cd35bcc430a2ee13b784ae344ac0f1b9d24a
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-05-20 20:56:50 -0400

    functionality complete

--- script.js
+++ script.js
@@ -17,28 +17,47 @@ function writePassword() {
 
   let allItems = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','1','2','3','4','5','6','7','8','9','!','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','',']','^','_','`','{','|','}','~']
   let allItemsDuplicate = allItems;
+  let lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  let uppercasseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  let numbers = ['1','2','3','4','5','6','7','8','9'];
+  let symbols = ['!','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','',']','^','_','`','{','|','}','~']
+  
   let store = [];
 
+  let counter = 0;
+
   if(wantsSpecilCharacters){
+      let index = Math.floor((Math.random() * symbols.length -1) + 1);  
+      password = password + symbols[index];
       store = store.concat(allItemsDuplicate.slice(61))
+      counter+=1;
   }
 
   if(wantsLowercase){
+      let index = Math.floor((Math.random() * lowercaseLetters.length -1) + 1);  
+      password = password + lowercaseLetters[index];
       store = store.concat(allItemsDuplicate.slice(0,26));
+      counter+=1;
       
   }
 
   if(wantsUppercase){
+      let index = Math.floor((Math.random() * uppercasseLetters.length -1) + 1);  
+      password = password + uppercasseLetters[index];
       store = store.concat(allItemsDuplicate.slice(26,52))
+      counter+=1;
       
   }
 
   if(wantNumbers){
+      let index = Math.floor((Math.random() * numbers.length -1) + 1);  
+      password = password + numbers[index];
       store = store.concat(allItemsDuplicate.slice(52,61))
+      counter+=1;
       
   }
 
-  for(let i=0; i<parseInt(wordCount);i++){
+ for(let i=0; i<parseInt(wordCount);i++){
     let index = Math.floor((Math.random() * store.length -1) + 1);
     password = password + store[index];
 
